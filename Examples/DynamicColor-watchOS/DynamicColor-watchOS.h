c38e621111ba97c7dac6e0b7f9127b5f2c8b8b25
“Henry <henry.hong@scmp.com>
2017-01-04 15:00:08 +0800

    added watchOS scheme and target

---
+++ Examples/DynamicColor-watchOS/DynamicColor-watchOS.h
@@ -0,0 +1,19 @@
+//
+//  DynamicColor-watchOS.h
+//  DynamicColor-watchOS
+//
+//  Created by henry.hong on 4/1/2017.
+//  Copyright © 2017 Yannick LORIOT. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for DynamicColor-watchOS.
+FOUNDATION_EXPORT double DynamicColor_watchOSVersionNumber;
+
+//! Project version string for DynamicColor-watchOS.
+FOUNDATION_EXPORT const unsigned char DynamicColor_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicColor_watchOS/PublicHeader.h>
+
+
